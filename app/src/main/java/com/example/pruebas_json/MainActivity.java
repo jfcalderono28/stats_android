78dab45b30f900ffa3fdf01adfda5446b97368e7
unknown <juanfelipecalderono@gmail.com>
2023-08-07 01:40:56 -0500

    muestra de estadisticas

--- app/src/main/java/com/example/pruebas_json/MainActivity.java
+++ app/src/main/java/com/example/pruebas_json/MainActivity.java
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import org.json.JSONException;
@@ -15,12 +17,14 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     EditText name_troop;
-    TextView stats;
     Button btn_search;
+    ListView lv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +32,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         name_troop = findViewById(R.id.name_troop);
-        stats = findViewById(R.id.stats);
+
         btn_search = findViewById(R.id.btn_Search);
+        lv1 = findViewById(R.id.lv1);
         btn_search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -44,70 +49,81 @@ public class MainActivity extends AppCompatActivity {
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
-                } else {
-                    stats.setText("No se encontró la tropa");
                 }
-
             }
         });
     }
 
+
+    //el metodo se llama barbarian pero va funcionar para cualquier tropa
     private void barbarian(String name) throws JSONException {
         String name_json = name;
+        List<String> llaves = new ArrayList<>();
+
         try {
             int resourceId = getResources().getIdentifier(name_json, "raw", getPackageName());
-            InputStream inputStream = getResources().openRawResource(resourceId);
+            if (resourceId != 0) {
 
-            byte[] buffer = new byte[inputStream.available()];
-            inputStream.read(buffer);
-            inputStream.close();
-            String json = new String(buffer, "UTF-8");
 
-            // Analizar el JSON
+                InputStream inputStream = getResources().openRawResource(resourceId);
 
-            JSONObject jsonObject = new JSONObject(json);
-            Log.d("MainActivity", String.valueOf(jsonObject));
-            Iterator<String> keys = jsonObject.keys();
+                byte[] buffer = new byte[inputStream.available()];
+                inputStream.read(buffer);
+                inputStream.close();
+                String json = new String(buffer, "UTF-8");
 
-            while (keys.hasNext()) {
-                String key = keys.next();
-                Object value = jsonObject.get(key);
+                // Analizar el JSON
 
-                Class value2 = value.getClass();
+                JSONObject jsonObject = new JSONObject(json);
+                Log.d("MainActivity", String.valueOf(jsonObject));
+                Iterator<String> keys = jsonObject.keys();
 
-                if (value2.equals(String.class)) {
-                    Log.d("keys", key);
-                    Log.d("value", (String) value);
-                } else if (value2.equals(JSONObject.class)) {
+                while (keys.hasNext()) {
+                    String key = keys.next();
+                    Object value = jsonObject.get(key);
+                    //esta linea es para saber que tipo de dato es value
+                    Class value2 = value.getClass();
 
-                    JSONObject nestedJsonObject = (JSONObject) value;
-                    // Ahora puedes trabajar con el objeto JSON nestedJsonObject
-                    // Por ejemplo, puedes obtener sus claves y valores
-                    Iterator<String> nestedKeys = nestedJsonObject.keys();
-                    while (nestedKeys.hasNext()) {
-                        String nestedKey = nestedKeys.next();
-                        Object nestedValue = nestedJsonObject.get(nestedKey);
-                        Log.d("nestedKey", nestedKey);
-                        Log.d("nestedValue", nestedValue.toString());
+                    if (key.equalsIgnoreCase("name") || key.equalsIgnoreCase("url")) {
+                        llaves.add(key + ":    " + (String) value);
+                    } else if (value2.equals(JSONObject.class)) {
+                        llaves.add("");
+                        //acá se llega a las estadisticas
+                        JSONObject nestedJsonObject = (JSONObject) value;
 
-                        JSONObject statsJsonObject = (JSONObject) nestedValue;
-                        Iterator<String> statsKeys = statsJsonObject.keys();
+                        Iterator<String> nestedKeys = nestedJsonObject.keys();
+                        while (nestedKeys.hasNext()) {
+                            //acá se entra a cada nivel de la tropa
+                            String nestedKey = nestedKeys.next();
+                            Object nestedValue = nestedJsonObject.get(nestedKey);
 
-                        while (statsKeys.hasNext()){
-                            String statKey = statsKeys.next();
-                            Object statValue = statsJsonObject.get(statKey);
-                            Log.d("statKey", statKey);
-                            Log.d("statValue", statValue.toString());
+
+                            JSONObject statsJsonObject = (JSONObject) nestedValue;
+                            Iterator<String> statsKeys = statsJsonObject.keys();
+                            while (statsKeys.hasNext()) {
+                                //Acá se recorre el json de las estadisticas
+                                String statKey = statsKeys.next();
+                                Object statValue = statsJsonObject.get(statKey);
+                                llaves.add(statKey + "  -->  " + (String) statValue);
+
+                                if (!statsKeys.hasNext()) {
+                                    llaves.add("");
+                                }
+                            }
                         }
-
+                    } else if (value.equals(null)) {
+                        //en caso de que las estadisticas sea null
+                        llaves.add("Sin estadisticas");
                     }
-                } else if (value.equals(null)) {
-                    stats.setText("Sin estadisticas");
                 }
+            } else {
+                llaves.add("No se encontró la tropa");
             }
+
         } catch (IOException ex) {
             throw new RuntimeException(ex);
         }
-        //stats.setText(String.valueOf(jsonObject));
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.lv1, llaves);
+        lv1.setAdapter(adapter);
     }
 }
